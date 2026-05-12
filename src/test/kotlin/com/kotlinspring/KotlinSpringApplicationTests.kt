8180aa2932f2c94a05ca146bb874978617be948f
나양선 <nyangsun@gmail.com>
2026-05-12 21:00:34 +0900

    Move integration tests from h2 to embedded postgres

--- src/test/kotlin/com/kotlinspring/KotlinSpringApplicationTests.kt
+++ src/test/kotlin/com/kotlinspring/KotlinSpringApplicationTests.kt
@@ -1,11 +1,14 @@
 package com.kotlinspring
 
+import com.kotlinspring.config.TestEmbeddedPostgresConfig
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.extensions.spring.SpringExtension
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@Import(TestEmbeddedPostgresConfig::class)
 @ActiveProfiles("test")
 class KotlinSpringApplicationTests : StringSpec() {
 
